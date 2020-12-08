52c1eb3e861386d408e2db7e0fc145ce7fce8789
gntorres <gntorres421@gmail.com>
2020-12-07 19:29:18 -0600

    commit tree 058c61b4fbb31e64217e878f23219081d3e339cf

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,1 +1,7 @@
-lean test 
+def introduction_with_language(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction(name)
+    puts "Hi, my name is #{name}."
+end
